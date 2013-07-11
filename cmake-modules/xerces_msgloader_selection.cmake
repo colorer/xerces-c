a0e8cab2cd65824cd8a31b0eeb7d1029de602f80
Aleksey Dobrunov <ctapmex@gmail.com>
2013-07-11 16:08:38 +0600

    build on ubuntu 13.04

--- cmake-modules/xerces_msgloader_selection.cmake
+++ cmake-modules/xerces_msgloader_selection.cmake
@@ -1,11 +1,19 @@
+# Check for inmemory msgloader
+set(msg_inmemory 1)
 
-set(t_inmemory 1)
+# Check for iconv support
+
+if(HAVE_NL_TYPES_H AND HAVE_CATOPEN AND HAVE_CATCLOSE AND HAVE_CATGETS)
+  set(msg_iconv 1)
+endif()
 # Check for platform-specific transcoders
-
 if(WIN32 OR MINGW OR MSYS)
-  set(t_win32 1)
+  set(msg_win32 1)
 endif()
 
-if(ENABLE_MSGLOADER_WIN32 AND t_win32)
+
+if(ENABLE_MSGLOADER_WIN32 AND msg_win32)
   set(XERCES_USE_MSGLOADER_WIN32 1)
+elseif(ENABLE_MSGLOADER_INMEMORY AND msg_inmemory)
+  set(XERCES_USE_MSGLOADER_INMEMORY 1)
 endif()
--- cmake-modules/xerces_netaccessor_selection.cmake
+++ cmake-modules/xerces_netaccessor_selection.cmake
@@ -6,22 +6,23 @@ CHECK_LIBRARY_EXISTS(nsl gethostbyname "" HAVE_LIBNSL)
 
 # whether we can support the sockets-based NetAccessor
 if (HAVE_SYS_SOCKET_H) 
-  set(t_curl 1)
+  set(net_socket 1)
 endif()
 
 # Check for platform-specific netaccessors
 # whether we can support the CFURL NetAccessor (Mac OS X)
 if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin" AND HAVE_CORESERVICES_CORESERVICES_H)
-  set(t_cfurl 1)
+  set(net_cfurl 1)
 endif()
 
 if((WIN32 OR MINGW OR MSYS) AND HAVE_WINSOCK2_H)
-  set(t_winsock 1)
+  set(net_winsock 1)
 endif()
 
 if(ENABLE_NETWORK)
-  if(ENABLE_NETACCESSOR_WINSOCK AND t_windows)
+  if(ENABLE_NETACCESSOR_WINSOCK AND net_windows)
     set(XERCES_USE_NETACCESSOR_WINSOCK 1)
+  elseif(ENABLE_NETACCESSOR_SOCKET AND net_socket)
+    set(XERCES_USE_NETACCESSOR_SOCKET 1)
   endif()
-
 endif(ENABLE_NETWORK)
