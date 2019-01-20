6906dc324a08be85898a8a60895037b71f1f780e
Aleksey Dobrunov <ctapmex@gmail.com>
2019-01-20 20:18:01 +0500

    typo

--- cmake/XercesWarnings.cmake
+++ cmake/XercesWarnings.cmake
@@ -33,61 +33,62 @@ option(fatal-warnings "Compiler warnings are errors" OFF)
 # flags, and enable them if supported.  This greatly improves the
 # quality of the build by checking for a number of common problems,
 # some of which are quite serious.
-if (show_warnings)
-  if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR
-     CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-    set(test_flags
-        -Wall
-        -Wcast-align
-        -Wcast-qual
-        -Wctor-dtor-privacy
-        -Wextra
-        -Wformat=2
-        -Wimplicit-atomic-properties
-        -Wmissing-declarations
-        -Wno-long-long
-        -Woverlength-strings
-        -Woverloaded-virtual
-        -Wredundant-decls
-        -Wreorder
-        -Wswitch-default
-        -Wunused-variable
-        -Wwrite-strings
-        -Wno-variadic-macros
-        -fstrict-aliasing)
-    if(extra-warnings)
-      list(APPEND test_flags
-          -Wfloat-equal
-          -Wmissing-prototypes
-          -Wunreachable-code)
-    endif()
-    if(fatal-warnings)
-      list(APPEND test_flags
-           -Werror)
-    endif()
-  elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
-    set(test_flags)
-    if(extra-warnings)
-      list(APPEND test_flags
-           /W4)
-    else()
-      list(APPEND test_flags
-           /W3)
-    endif()
-    if (fatal-warnings)
-      list(APPEND test_flags
-           /WX)
-    endif()
-  endif()
-
-  include(CheckCXXCompilerFlag)
-
-  foreach(flag ${test_flags})
-    string(REGEX REPLACE "[^A-Za-z0-9]" "_" flag_var "${flag}")
-    set(test_c_flag "CXX_FLAG${flag_var}")
-    CHECK_CXX_COMPILER_FLAG(${flag} "${test_c_flag}")
-    if (${test_c_flag})
-       set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${flag}")
-    endif (${test_c_flag})
-  endforeach(flag ${test_flags})
+if (NOT show_warnings)
+  return()
 endif ()
+if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR
+    CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+  set(test_flags
+      -Wall
+      -Wcast-align
+      -Wcast-qual
+      -Wctor-dtor-privacy
+      -Wextra
+      -Wformat=2
+      -Wimplicit-atomic-properties
+      -Wmissing-declarations
+      -Wno-long-long
+      -Woverlength-strings
+      -Woverloaded-virtual
+      -Wredundant-decls
+      -Wreorder
+      -Wswitch-default
+      -Wunused-variable
+      -Wwrite-strings
+      -Wno-variadic-macros
+      -fstrict-aliasing)
+  if (extra-warnings)
+    list(APPEND test_flags
+        -Wfloat-equal
+        -Wmissing-prototypes
+        -Wunreachable-code)
+  endif ()
+  if (fatal-warnings)
+    list(APPEND test_flags
+        -Werror)
+  endif ()
+elseif (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+  set(test_flags)
+  if (extra-warnings)
+    list(APPEND test_flags
+        /W4)
+  else ()
+    list(APPEND test_flags
+        /W3)
+  endif ()
+  if (fatal-warnings)
+    list(APPEND test_flags
+        /WX)
+  endif ()
+endif ()
+
+include(CheckCXXCompilerFlag)
+
+foreach (flag ${test_flags})
+  string(REGEX REPLACE "[^A-Za-z0-9]" "_" flag_var "${flag}")
+  set(test_c_flag "CXX_FLAG${flag_var}")
+  CHECK_CXX_COMPILER_FLAG(${flag} "${test_c_flag}")
+  if (${test_c_flag})
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${flag}")
+  endif (${test_c_flag})
+endforeach (flag ${test_flags})
